6122a21fa196c1533b96c2324dae3ab262d5ab98
muegan <2216869@sookmyung.ac.kr>
2023-10-12 18:22:47 +0900

    4번

--- main.c
+++ main.c
@@ -3,42 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int factorial(int n)
+int square(int a)
 {
-	int res=1;
-	int i;
-	
-	for(i=1; i<=n; i++)
-		res=res*i;
-		
-	return res; 
-}
-
-int combination(int n, int r)
-{
-	int div1, div2;
-	
-	div1=factorial(n);
-	div2=factorial(n-r)*factorial(r);
-	return (div1/div2);//factorial 함수로 수식 계산	
+	return (a*a);
 }
 
 int main(int argc, char *argv[]) {
 	
-	int n, r;
-	int div1, div2;
-	int result;
-	
-	printf("input n : ");
-	scanf("%d", &n); 
-	
-	printf("input r : ");
-	scanf("%d", &r); 
-	
-	
-	result=combination(n,r);
-	
-	printf("result is %d\n", result);
-	
-	return 0; 
+	int a=2;
+	a=square(a);
+	printf("a=%i\n", a);
 }
