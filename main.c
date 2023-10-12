f724ff56135fc4a885be41c071ce77ef5f5bc704
muegan <2216869@sookmyung.ac.kr>
2023-10-12 15:57:36 +0900

    3-3번

--- main.c
+++ main.c
@@ -3,18 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int n)
+int get_max(int x, int y)
 {
-	int result;
-	result=n*n;
-	printf("result is %d", result);
+	if(x>y)
+		printf("result is %d", x);
+	else
+		printf("result is %d", y);
 	
 }
 
 int main(int argc, char *argv[]) {
 	
-	int n;
-	square(3);
+	int x, y;
+	get_max(3,7);
 	
 	return 0;
 
